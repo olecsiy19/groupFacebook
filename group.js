a313cab7903999f435eaebff798cf1f2755872b2
olecsiy19 <olecsiy19@gmail.com>
2016-06-01 14:26:27 +0300

    Add files via upload

--- group.js
+++ group.js
@@ -1,17 +1,31 @@
-function facebookGroup() {
+function removeGroup() {
+    searchGroup();
+    var group = document.getElementsByClassName('_1yt');
+    if(document.getElementsByClassName('phm').length == 0) {
+        for(var i = 0; i < group.length; ++i) {
+            group[i].innerHTML = '';
+        }
+        window.scrollBy(0,10);
+        window.scrollBy(0,-10);
+        setTimeout(removeGroup, 100);
+    }
+    else {
+        console.log(JSON.stringify(J));
+    }
+}
+
+function searchGroup() {
     var facebookGroup = document.getElementsByClassName('_gli');
     var jsonArray = [];
     for (var i = 0; i < facebookGroup.length; ++i) {
         var t = new Object();
+        var data = facebookGroup[i].querySelector('div');
+        t.ID = JSON.parse(data.parentNode.dataset.bt);
+        t.ID = t.ID.id;
         t.nameGroup = facebookGroup[i].querySelector('._gll a').textContent;
         t.group = facebookGroup[i].querySelector('div ._pac').textContent;
-        var str = facebookGroup[i].querySelector('._glo div ._ajw:first-child ._52eh').textContent;
-        t.members = '';
-        for(var j = 0; j < str.length; ++j) {
-            if(str[j] != ',') {
-                t.members += str[j];
-            }
-        }
+        t.members = facebookGroup[i].querySelector('._glo div ._ajw:first-child ._52eh').textContent;
+        t.members = t.members.replace(/,/g,"")
         t.members = parseInt(t.members);
         t.follower = facebookGroup[i].querySelector('._glj ._42ft').textContent;
         if(t.follower == 'Joined') {
@@ -22,7 +36,12 @@ function facebookGroup() {
         }
         jsonArray.push(t);
     }
-    console.log(jsonArray);
-    return JSON.stringify(jsonArray);
+   if(jsonArray.length !=0) {
+        for(var i = 0; i <jsonArray.length; ++i) {
+            J.push(jsonArray[i]);
+        }
+    }
 }
-facebookGroup();
+var J = [];
+removeGroup();
+
