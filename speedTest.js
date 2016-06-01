a33935760b1641075fdba23d720af7db0e10fe02
olecsiy19 <olecsiy19@gmail.com>
2016-06-01 14:26:06 +0300

    Add files via upload

--- speedTest.js
+++ speedTest.js
@@ -1,76 +1,32 @@
-console.time('test');
-for(var k = 0; k<10000; ++k) {
-    function facebookGroup() {
-        var facebookGroup = document.getElementsByClassName('_gli');
-        var jsonArray = [];
-        for(var i = 0; i < facebookGroup.length; ++i) {
-            var t = new Object();
-            var str = facebookGroup[i].querySelector('._glo div ._ajw:first-child ._52eh').textContent;
-            t.members = '';
-            for(var j = 0; j < str.length; ++j) {
-                if(str[j] != ',') {
-                    t.members += str[j];
-                }
+
+    var str = "fgdfgsdg,dgrefeerf";
+    var t;
+
+    console.time('test');
+    for(var k = 0; k<10000; ++k) {
+        t = '';
+        for(var j = 0; j < str.length; ++j) {
+            if(str[j] != ',') {
+                t += str[j];
             }
-            t.members = parseInt(t.members);
-            jsonArray.push(t);
         }
-       // console.log(jsonArray);
     }
-    facebookGroup();
-}
-console.timeEnd('test');
+    console.timeEnd('test');
 
-console.time('test');
-for(var k = 0; k<10000; ++k) {
-    function facebookGroup() {
-        var facebookGroup = document.getElementsByClassName('_gli');
-        var jsonArray = [];
-        for(var i = 0; i < facebookGroup.length; ++i) {
-            var t = new Object();
-            t.members = facebookGroup[i].querySelector('._glo div ._ajw:first-child ._52eh').textContent;
-            t.members = t.members.replace(/,/g,"")
-            t.members = parseInt(t.members);
-            jsonArray.push(t);
-        }
-        //console.log(jsonArray);
+    console.time('test');
+    for(var k = 0; k<10000; ++k) {
+        t = str.replace(/,/g,"")
     }
-    facebookGroup();
-}
-console.timeEnd('test');
+    console.timeEnd('test');
 
-console.time('test');
-for(var k = 0; k<10000; ++k) {
-    function facebookGroup() {
-        var facebookGroup = document.getElementsByClassName('_gli');
-        var jsonArray = [];
-        for(var i = 0; i < facebookGroup.length; ++i) {
-            var t = new Object();
-            t.members = facebookGroup[i].querySelector('._glo div ._ajw:first-child ._52eh').textContent;
-            t.members = t.members.replace(",","")
-            t.members = parseInt(t.members);
-            jsonArray.push(t);
-        }
-       // console.log(jsonArray);
+    console.time('test');
+    for(var k = 0; k<10000; ++k) {
+        t.members = str.replace(",","")
     }
-    facebookGroup();
-}
-console.timeEnd('test');
+    console.timeEnd('test');
 
-console.time('test');
-for(var k = 0; k<10000; ++k) {
-    function facebookGroup() {
-        var facebookGroup = document.getElementsByClassName('_gli');
-        var jsonArray = [];
-        for(var i = 0; i < facebookGroup.length; ++i) {
-            var t = new Object();
-            t.members = facebookGroup[i].querySelector('._glo div ._ajw:first-child ._52eh').textContent;
-            t.members = t.members.replace(/(\w+)\,(\w+)/,"$1$2")
-            t.members = parseInt(t.members);
-            jsonArray.push(t);
-        }
-        //console.log(jsonArray);
+    console.time('test');
+    for(var k = 0; k<10000; ++k) {
+        t.members = str.replace(/(\w+)\,(\w+)/,"$1$2")
     }
-    facebookGroup();
-}
-console.timeEnd('test');
+    console.timeEnd('test');
