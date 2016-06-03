f40dde3ddd1945402a2cace2e841f7cd5c2ccc13
olecsiy19 <olecsiy19@gmail.com>
2016-06-03 18:23:59 +0300

    Add files via upload

--- group.js
+++ group.js
@@ -1,47 +1,51 @@
-function removeGroup() {
-    searchGroup();
-    var group = document.getElementsByClassName('_1yt');
-    if(document.getElementsByClassName('phm').length == 0) {
-        for(var i = 0; i < group.length; ++i) {
-            group[i].innerHTML = '';
+function searchAndRemoveGroups() {
+    var facebookGroups = document.getElementsByClassName('_gli');
+    var endOfResults = document.getElementsByClassName('phm').length;
+    pushGroupsOnScreen(facebookGroups);
+    if(endOfResults == 0) {
+        for(var i = 0; i < facebookGroups.length; ++i) {
+            facebookGroups[i].outerHTML = '';
         }
         window.scrollBy(0,10);
         window.scrollBy(0,-10);
-        setTimeout(removeGroup, 100);
+        setTimeout(searchAndRemoveGroups, 100);
     }
     else {
-        console.log(JSON.stringify(J));
+       console.log(JSON.stringify(informationAboutGroups));
     }
 }
 
-function searchGroup() {
-    var facebookGroup = document.getElementsByClassName('_gli');
-    var jsonArray = [];
-    for (var i = 0; i < facebookGroup.length; ++i) {
-        var t = new Object();
-        var data = facebookGroup[i].querySelector('div');
-        t.ID = JSON.parse(data.parentNode.dataset.bt);
-        t.ID = t.ID.id;
-        t.nameGroup = facebookGroup[i].querySelector('._gll a').textContent;
-        t.group = facebookGroup[i].querySelector('div ._pac').textContent;
-        t.members = facebookGroup[i].querySelector('._glo div ._ajw:first-child ._52eh').textContent;
-        t.members = t.members.replace(/,/g,"")
-        t.members = parseInt(t.members);
-        t.follower = facebookGroup[i].querySelector('._glj ._42ft').textContent;
-        if(t.follower == 'Joined') {
-            t.follower = true;
-        }
-        else {
-            t.follower = false;
-        }
-        jsonArray.push(t);
+function pushGroupsOnScreen(facebookGroups) {
+    var informationAboutGroupsOnScreen = [];
+    for (var i = 0; i < facebookGroups.length; ++i) {
+        informationAboutGroupsOnScreen.push(parseGroupInformationByElement(facebookGroups[i]));
     }
-   if(jsonArray.length !=0) {
-        for(var i = 0; i <jsonArray.length; ++i) {
-            J.push(jsonArray[i]);
+   if(informationAboutGroupsOnScreen.length !=0) {
+        for(var i = 0; i <informationAboutGroupsOnScreen.length; ++i) {
+            informationAboutGroups.push(informationAboutGroupsOnScreen[i]);
         }
     }
 }
-var J = [];
-removeGroup();
 
+function parseGroupInformationByElement(facebookGroup) {
+    var informationAboutGroup = new Object();
+    var searchIdByDataAtribute = facebookGroup.querySelector('div');
+    informationAboutGroup.ID = JSON.parse(searchIdByDataAtribute.parentNode.dataset.bt);
+    informationAboutGroup.ID = informationAboutGroup.ID.id;
+    informationAboutGroup.nameGroup = facebookGroup.querySelector('._gll a').textContent;
+    informationAboutGroup.group = facebookGroup.querySelector('div ._pac').textContent;
+    informationAboutGroup.members = facebookGroup.querySelector('._glo div ._ajw:first-child ._52eh').textContent;
+    informationAboutGroup.members = informationAboutGroup.members.replace(/,/g,"")
+    informationAboutGroup.members = parseInt(informationAboutGroup.members);
+    informationAboutGroup.follower = facebookGroup.querySelector('._glj ._42ft').textContent;
+    if(informationAboutGroup.follower == 'Joined') {
+        informationAboutGroup.follower = true;
+    }
+    else {
+        informationAboutGroup.follower = false;
+    }
+    return informationAboutGroup;
+}
+
+var informationAboutGroups = [];
+searchAndRemoveGroups();
